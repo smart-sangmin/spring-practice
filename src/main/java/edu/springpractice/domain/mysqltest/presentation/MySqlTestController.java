2fa280316ca1fbdc078ae63edaf19efed15c0d71
sangminnim <sangmins930@gmail.com>
2023-08-27 23:56:56 +0900

    feat: mysqltest 완성

--- src/main/java/edu/springpractice/domain/mysqltest/presentation/MySqlTestController.java
+++ src/main/java/edu/springpractice/domain/mysqltest/presentation/MySqlTestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
-@RequestMapping("/mysql")
+@RequestMapping("/api/mysql")
 @RequiredArgsConstructor
 public class MySqlTestController {
 
